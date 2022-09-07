cf7530933b698561ba2fceb738fa8dda796d4ccd
lastrix <2@lastrix.org>
2022-09-07 10:03:34 +0300

    FIX: disable test FIX: spring boot plugin should be disabled for base

--- scp-write-sender-service-app/src/test/java/com/lastrix/scp/writesender/ScpWriteSenderServiceApplicationTests.java
+++ scp-write-sender-service-app/src/test/java/com/lastrix/scp/writesender/ScpWriteSenderServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package com.lastrix.scp.writesender;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class ScpWriteSenderServiceApplicationTests {
 
 	@Test
